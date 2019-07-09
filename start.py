f8bb6b0f1c0a87f1a5e7ebaff717f2525434fea7
Azharuddin <azhargiri@gmail.com>
2019-07-09 17:07:52 +0700

    init

---
+++ start.py
@@ -0,0 +1,106 @@
+from selenium import webdriver
+from selenium.common.exceptions import TimeoutException
+from selenium.webdriver.support.ui import WebDriverWait # available since 2.4.0
+from selenium.webdriver.support import expected_conditions as EC # available since 2.26.0
+from selenium.webdriver.common.by import By
+import time
+
+
+
+class instagram(object):
+    url = 'https://www.instagram.com'
+    username = ""
+    login_url = url + '/accounts/login'
+    password = ""
+    
+instagram.username = 'bebekracing.id'
+instagram.password = 'bebekracing28'
+
+likeListSelector='a.zV_Nj'
+likerSelector='div.Igw0E.rBNOH.eGOV_.ybXk5._4EzTm.XfCBB.HVWg4._0mzm-.ZUqME'
+# likeSelector='div.Igw0E.rBNOH.eGOV_.ybXk5._4EzTm.XfCBB.HVWg4._0mzm-.ZUqME'
+likerButtonSelector='button[type=button]._0mzm-.sqdOP.L3NKy'
+
+
+urlCollection = [
+        "https://www.instagram.com/p/BzsK9HZgIYm/",
+        "https://www.instagram.com/p/BzsQCJKhP-d/",
+        "https://www.instagram.com/p/BzsCOoDhMMt/"
+        ]
+
+# Create a new instance of the Firefox driver
+# driverOptions = webdriver.FirefoxOptions()
+# driverOptions.add_experimental_option('prefs', {'intl.accept_languages': 'en, en_US'})
+# driver = webdriver.Firefox(firefox_options = driverOptions)
+driver = webdriver.Firefox()
+
+# go to the google home page
+driver.get(instagram.login_url)
+
+driver.implicitly_wait(10)
+
+# find the element that's name attribute is q (the google search box)
+inputUsernameLogin = driver.find_element_by_css_selector('input[name=username]')
+inputPasswordLogin = driver.find_element_by_css_selector('input[name=password]')
+submitLoginButton = driver.find_element_by_css_selector('button[type=submit]')
+
+inputUsernameLogin.send_keys(instagram.username)
+inputPasswordLogin.send_keys(instagram.password)
+
+driver.implicitly_wait(2)
+
+submitLoginButton.click()
+
+driver.implicitly_wait(10)
+time.sleep(10)
+
+# WebDriverWait(driver, 20).until(
+#         EC.presence_of_element_located((By.TAG_NAME, 'body'))
+#         )
+
+for url in urlCollection:
+    driver.get(url)
+    WebDriverWait(driver, 20).until(
+            EC.presence_of_element_located((By.TAG_NAME, 'body'))
+            )
+
+    likeListButton= driver.find_element_by_css_selector(likeListSelector)
+    likeListButton.click()
+
+    WebDriverWait(driver, 20).until(
+            EC.presence_of_element_located((By.CSS_SELECTOR, 'div[role=dialog].pbNvD.fPMEg'))
+            )
+
+    likerDialog = driver.find_element_by_css_selector('div[role=dialog].pbNvD.fPMEg')
+    likerList = likerDialog.find_elements_by_css_selector(likerSelector)
+    
+    max_liker_count = 3
+    n = 0
+    
+    while(n < max_liker_count):
+        liker = likerList[n]
+        followButton = liker.find_element_by_css_selector(likerButtonSelector)
+        accountToFollow = liker.find_elements_by_css_selector('a')[0]
+
+        if followButton.text == 'Follow' or followButton.text == 'Ikuti':
+            followButton.click()
+            print('Following %s' % accountToFollow.text)
+            # url = 'https://www.instagram.com/' + accountToFollow.text
+            # driver.get(url)
+            # driver.implicitly_wait(5)
+
+            # peopleFollowButton = driver.find_elements_by_css_selector('button')[0]
+
+            # if peopleFollowButton.text == 'Ikuti' or peopleFollowButton.text == 'Follow':
+            #     peopleFollowButton.click()
+            # else:
+            #     print('This people %s is already followed', accountToFollow.text)
+
+            driver.implicitly_wait(5)
+        else:
+            print('Already following this user %s (link: %s)', accountToFollow.text, accountToFollow.text)
+            driver.implicitly_wait(2)
+            max_liker_count = max_liker_count + 1
+
+        n = n + 1 
+        print('------')
